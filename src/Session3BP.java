cf12d6389bb4683aed0e4e1a7388881a93acd76e
supratimb237 <supratim237@gmail.com>
2025-12-26 21:56:13 +0530

    Added code changes for various functional interfaces

---
+++ src/Session3BP.java
@@ -0,0 +1,35 @@
+import java.util.List;
+import java.util.function.Function;
+import java.util.function.Predicate;
+
+// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
+// then press Enter. You can now see whitespace characters in your code.
+public class Session3BP {
+    public static void main(String[] args) {
+
+        //Sum of all the nos in a list
+        List<Integer> numbers = List.of(1, 12, 15, 12, 6, 7, 8, 9, 1, 76);
+
+        //Trying Behaviour Parameterization
+
+        //Predicate<Integer> integerPredicate = x -> x % 2 == 0;
+        //This is behaviour parameterization -> we are directly passing the lambda expression in the method params
+        //To print the even nos
+        filterNumbersByRule(numbers, x -> x%2==0);
+        //To print the odd nos
+        filterNumbersByRule(numbers, x -> x%2!=0);
+
+        //Function<Integer, Integer> integerIntegerFunction = x -> x * x;
+        //Using Behaviour Parameterization for Maps
+        mapTheNumbersBasedOnBehaviour(numbers, x -> x * x);
+    }
+
+    private static void mapTheNumbersBasedOnBehaviour(List<Integer> numbers, Function<Integer, Integer> integerIntegerFunction) {
+        List<Integer> collect = numbers.stream().map(integerIntegerFunction).toList();
+        System.out.println(collect);
+    }
+
+    private static void filterNumbersByRule(List<Integer> numbers, Predicate<Integer> integerPredicate) {
+        numbers.stream().filter(integerPredicate).forEach(System.out::println);
+    }
+}
---
+++ src/Session3FI.java
@@ -0,0 +1,56 @@
+import java.util.List;
+import java.util.Random;
+import java.util.function.*;
+
+public class Session3FI {
+
+
+    public static void main(String[] args) {
+
+        List<Integer> numbers = List.of(1, 5, 1, 12, 12, 15, 16, 8, 9, 8, 16, 7, 2, 3, 8, 10);
+
+        //A Predicate is used to do an operation that returns a boolean value
+        //<Integer> --> The input is an Integer
+        Predicate<Integer> integerPredicate = x -> x % 2 == 0;
+
+        //A Function is used to perform any operation on an element
+        //<Integer, Integer> --> Both the input and output are Integers
+        Function<Integer, Integer> integerIntegerFunction = x -> x * x;
+
+
+        //This is used in case of performing operations on two elements
+        //Here, <Integer> --> The type of Input vars are same as output
+        BinaryOperator<Integer> integerBinaryOperator = (x, y) -> x + y;
+
+        //Consumer
+        //Takes some input but result is void i.e. to store this value i.e. no return type
+        Consumer<Integer> consumer = System.out::println;
+
+        //Supplier
+        //Here, no inputs are provided, but we still get an output
+        //Factory pattern
+        Supplier<Integer> supplier = () -> {
+            Random random = new Random();
+            return random.nextInt(100);
+        };
+
+        //Unary Operator
+        //Operates on only 1 variable/parameter
+        UnaryOperator<Integer> unaryOperator = x -> x*3;
+
+        //BiPredicate
+        BiPredicate<Integer,String> biPredicate = (x,s) -> {
+            return x>10 && s.equalsIgnoreCase("test");
+        };
+
+        
+
+
+
+
+        System.out.println(numbers.stream()
+                .filter(integerPredicate)
+                .map(integerIntegerFunction)
+                .reduce(0, integerBinaryOperator));
+    }
+}
